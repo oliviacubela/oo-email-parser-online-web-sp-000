7c7b80b6383e659eb8d4d68fc860d44a1b899dc5
Olivia Cubela <oliviacubela@gmail.com>
2020-07-10 01:41:46 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,6 +1,16 @@
 class EmailAddressParser
 
   attr_accessor :email_addresses 
+  @@all = []
+
+  def initialize(email_addresses)
+    @email_addresses = email_addresses
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end 
 
 
 end
