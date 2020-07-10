1addf598da6e8643358c7c9becf873cc9c3a4395
Olivia Cubela <oliviacubela@gmail.com>
2020-07-10 02:31:29 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,8 +3,7 @@ class EmailAddressParser
   attr_accessor :name, :csv_emails
 
   def initialize(csv_emails)
-#   # you don't need name in here, it is redundant/
-#   @name = name
+    @name = name
     @csv_emails = csv_emails
   end
 
