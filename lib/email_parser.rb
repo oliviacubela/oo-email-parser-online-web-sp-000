1e23727ee0fed7151618a06fae9c2f064b8b62dd
Olivia Cubela <oliviacubela@gmail.com>
2020-07-10 01:12:17 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,22 @@
-# Build a class EmailAddressParser that accepts a string of unformatted 
+class EmailAddressParser
+
+  attr_accessor :email_addresses :csv_emails
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+# Build a class EmailAddressParser that accepts a string of unformatted
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
