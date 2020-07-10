168f9ed7faa230eaa232998fc8aa8861882ba686
Olivia Cubela <oliviacubela@gmail.com>
2020-07-10 02:30:11 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,25 +1,25 @@
-class EmailParser 
+class EmailParser
   
   attr_accessor :name, :csv_emails
-  
+
   def initialize(csv_emails)
 #   # you don't need name in here, it is redundant/
 #   @name = name
     @csv_emails = csv_emails
-  end 
+  end
 
-# This will not work because "emails2" below will not be properly parsed  
-# def parse 
+# This will not work because "emails2" below will not be properly parsed
+# def parse
 #   csv_emails.split(/\s*,\s*/)
-# end 
+# end
 
-  def parse 
+  def parse
     csv_emails.split.collect do |address|
-      address.split(',') 
+      address.split(',')
     end
-    .flatten.uniq 
-  end 
-end 
+    .flatten.uniq
+  end
+end
 
 
 
